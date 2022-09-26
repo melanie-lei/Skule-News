1381fef7f11cff162c84983174100aeee6b433bc
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2022-09-26 11:27:18 -0400

    firebase

--- source code/admin_panel/lib/helper/constant_util.dart
+++ source code/admin_panel/lib/helper/constant_util.dart
@@ -2,7 +2,7 @@ class AppConfig {
   static String projectName = 'Blog Master';
   static String projectTagline = 'Welcome to Blog Master, enjoy the best blogs';
 
-  static String firebaseStorageBucketUrl = 'musicy-46533.appspot.com';
+  static String firebaseStorageBucketUrl = 'skule-news.appspot.com';
 
     static String backgroundImage =
       'https://images.unsplash.com/photo-1655212681194-fd1932c9b542?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60';
