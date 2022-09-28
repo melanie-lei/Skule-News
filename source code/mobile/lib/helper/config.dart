8a4eca58d10e2c2adbafb6fde16c121631d2c4a3
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2022-09-28 10:05:58 -0400

    changed ID root to local

--- source code/mobile/lib/helper/config.dart
+++ source code/mobile/lib/helper/config.dart
@@ -1,7 +1,7 @@
 import 'package:music_streaming_mobile/helper/common_import.dart';
 
 class AppConfig {
-  static String projectName = 'BlogMaster'.tr();
+  static String projectName = 'Skule News'.tr();
 
   static String dummyProfilePictureUrl =
       'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60';
@@ -13,10 +13,13 @@ class AppConfig {
 
 class AdmobConstants {
   // Admob ids for android
-  static const bannerAdUnitIdForAndroid = 'ca-app-pub-3940256099942544/6300978111';
-  static const interstitialAdUnitIdForAndroid = 'ca-app-pub-3940256099942544/1033173712';
+  static const bannerAdUnitIdForAndroid =
+      'ca-app-pub-3940256099942544/6300978111';
+  static const interstitialAdUnitIdForAndroid =
+      'ca-app-pub-3940256099942544/1033173712';
 
   // Admob ids for iOS
   static const bannerAdUnitIdForiOS = 'ca-app-pub-3940256099942544/6300978111';
-  static const interstitialAdUnitIdForiOS = 'ca-app-pub-3940256099942544/1033173712';
+  static const interstitialAdUnitIdForiOS =
+      'ca-app-pub-3940256099942544/1033173712';
 }
