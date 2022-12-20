892a0542341d5d884e40d178aa9846c85b28797f
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2022-12-20 11:29:42 -0500

    common config update

--- source code/mobile/lib/common_config.dart
+++ source code/mobile/lib/common_config.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:skule_news_mobile/helper/common_import.dart';
 
 class CommonConfig {
-  static String schoolName = "Richmond Hill High School".tr();
+  static String schoolName = "".tr();
   static Color primaryColor = const Color(0xff3E6553);
   static Color buttonTextColor = const Color(0xffffffff);
+
+  // Firebase app config.
+  static String apiKey = "";
+  static String authDomain = "";
+  static String projectId = "";
+  static String storageBucket = "";
+  static String messagingSenderId = "";
+  static String appId = "";
+  static String measurementId = "";
 }
