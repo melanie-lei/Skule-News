a1559e492cc6d91ab65f830f31603aff9be4e4ee
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2022-12-21 09:19:36 -0500

    common template config

---
+++ source code/admin_panel/lib/common_config_template.dart
@@ -0,0 +1,19 @@
+/* RENAME TO common_config.dart */
+
+import 'package:flutter/material.dart';
+import 'helper/common_import.dart';
+
+class CommonConfig {
+  static String schoolName = "".tr();
+  static Color primaryColor = const Color(0xffdbc1c1);
+  static Color buttonTextColor = const Color(0xff1c1c1c);
+
+  // Firebase app config.
+  static String apiKey = "";
+  static String authDomain = "";
+  static String projectId = "";
+  static String storageBucket = "";
+  static String messagingSenderId = "";
+  static String appId = "";
+  static String measurementId = "";
+}
---
+++ source code/mobile/lib/common_config_template.dart
@@ -0,0 +1,19 @@
+/* RENAME TO common_config.dart */
+
+import 'package:flutter/material.dart';
+import 'package:skule_news_mobile/helper/common_import.dart';
+
+class CommonConfig {
+  static String schoolName = "".tr();
+  static Color primaryColor = const Color(0xff3E6553);
+  static Color buttonTextColor = const Color(0xffffffff);
+
+  // Firebase app config.
+  static String apiKey = "";
+  static String authDomain = "";
+  static String projectId = "";
+  static String storageBucket = "";
+  static String messagingSenderId = "";
+  static String appId = "";
+  static String measurementId = "";
+}
