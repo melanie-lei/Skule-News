0ce0e007694c132deda20ad5939e1b376a181788
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2022-12-22 09:02:57 -0500

    rhhs name

--- source code/mobile/lib/common_config.dart
+++ source code/mobile/lib/common_config.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skule_news_mobile/helper/common_import.dart';
 
 class CommonConfig {
-  static String schoolName = "".tr();
+  static String schoolName = "Richmond Hill High School".tr();
   static Color primaryColor = const Color(0xff3E6553);
   static Color buttonTextColor = const Color(0xffffffff);
 
