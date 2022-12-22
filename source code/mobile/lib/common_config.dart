e5f5c93491985b6dd8172818c42e74ba083fc81b
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2022-12-22 09:05:16 -0500

    test branch change

--- source code/mobile/lib/common_config.dart
+++ source code/mobile/lib/common_config.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skule_news_mobile/helper/common_import.dart';
 
 class CommonConfig {
+  // making a change to main branch
   static String schoolName = "".tr();
   static Color primaryColor = const Color(0xff3E6553);
   static Color buttonTextColor = const Color(0xffffffff);
