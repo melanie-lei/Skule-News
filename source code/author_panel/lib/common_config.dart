5ba2e289d2c88fbda02569c8cac4da14089069f4
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2023-01-10 09:37:53 -0500

    common config template to common config

--- source code/author_panel/lib/common_config.dart
+++ source code/author_panel/lib/common_config.dart
@@ -6,12 +6,12 @@ class CommonConfig {
   static Color primaryColor = const Color(0xffdbc1c1);
   static Color buttonTextColor = const Color(0xff1c1c1c);
 
-    // Firebase app config.
-  static String apiKey = "";
-  static String authDomain = "";
-  static String projectId = "";
-  static String storageBucket = "";
-  static String messagingSenderId = "";
-  static String appId = "";
-  static String measurementId = "";
+  static FirebaseOptions options = const FirebaseOptions(
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: "");
 }
